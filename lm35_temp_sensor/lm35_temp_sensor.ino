81bb741f88ec9599211bedf612ac0085a56d9508
Austinstevesk <austinstevesk@gmail.com>
2021-02-01 19:32:01 +0300

    Added lm35 temperature sensor

---
+++ lm35_temp_sensor/lm35_temp_sensor.ino
@@ -0,0 +1,42 @@
+int led = 4;
+float sinVal;
+int toneVal;
+unsigned long tepTimer;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(8, OUTPUT);
+pinMode(led, OUTPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int val;
+  double data;
+  val = analogRead(0);
+  data = (double)val*(5/10.24); //Convert voltage to temperature value
+
+  if(data>25){
+    digitalWrite(led, HIGH);
+   
+    for(int x = 0; x<180; x++){
+      sinVal = (sin(x*(3.142/180)));
+      toneVal = 2000+(int(sinVal*1000));
+  
+      tone(8, toneVal);
+      delay(2);
+  }
+  }else{
+    digitalWrite(led, LOW);
+    noTone(8);
+  }
+  //every 500 millis
+  if(millis()-tepTimer>50){
+    tepTimer = millis();
+    Serial.print("Temp: ");
+    Serial.print(data);
+    Serial.println("C");
+  }
+
+}
