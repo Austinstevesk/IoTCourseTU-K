a95cb377fa59ec83093605b4a93a724ef8f72d4f
Austinstevesk <austinstevesk@gmail.com>
2021-02-16 23:40:32 +0300

    Added a dht module to the thingspeakintro

--- thingspeakintro/thingspeakintro.ino
+++ thingspeakintro/thingspeakintro.ino
@@ -71,6 +71,14 @@ void loop()
   float f = dht.readTemperature(tr// Wait a few seconds between measurements.
   delay(2000);
 
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);ue);
+
 
 
 
